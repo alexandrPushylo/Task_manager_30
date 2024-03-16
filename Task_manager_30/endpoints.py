06591493e9dbf29786b28a1085cdc2d470dd5f9f
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-03-16 15:05:43 +0300

    Create endpoints.py

---
+++ Task_manager_30/endpoints.py
@@ -0,0 +1,14 @@
+TECHNICS = '/technics/'
+EDIT_TECHNIC = '/edit_technic/'
+# DELETE_TECHNIC = '/delete_technic/'
+LOGIN = '/login/'
+
+
+ERROR = '#'
+
+USERS = '/users/'
+EDIT_USERS = '/edit_users/'
+DELETE_USERS = '/delete_users/'
+
+
+
