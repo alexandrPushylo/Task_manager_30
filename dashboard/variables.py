d50d4f6353da34a3e59dda25abb3fe473847647a
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-06-16 23:34:15 +0300

    Create variables.py

---
+++ dashboard/variables.py
@@ -0,0 +1,17 @@
+# template = {'title': '', 'name': ''}
+
+VAR_APPLICATION_SEND = {
+    'title': 'Отметка отправки приложений',
+    'name': 'apps_status_send'
+}
+VAR_DEFAULT_PASSWORD = {
+    'title': 'Пароль по умолчанию',
+    'name': 'default_password',
+    'value': '1234',
+}
+
+# Для prepare_variables() - automatic creation of variables
+VARIABLES_LIST = (
+    # VAR_APPLICATION_SEND,
+    VAR_DEFAULT_PASSWORD,
+)
