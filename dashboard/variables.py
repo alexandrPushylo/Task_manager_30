e6c8d7e03f3ce9cce62b94359b1e733eb6e506fb
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-06-18 00:14:58 +0300

    add var: 'time_reception_of_applications'

--- dashboard/variables.py
+++ dashboard/variables.py
@@ -1,4 +1,5 @@
 # template = {'title': '', 'name': ''}
+from datetime import datetime
 
 VAR_APPLICATION_SEND = {
     'title': 'Отметка отправки приложений',
@@ -9,9 +10,16 @@ VAR_DEFAULT_PASSWORD = {
     'name': 'default_password',
     'value': '1234',
 }
+VAR_TIME_RECEPTION_OF_APPS = {
+    'title': 'Время приема заявок',
+    'name': 'time_reception_of_applications',
+    'time': datetime.now().time().replace(hour=16, minute=0, second=0, microsecond=0),
+    'flag': True,
+}
 
 # Для prepare_variables() - automatic creation of variables
 VARIABLES_LIST = (
     # VAR_APPLICATION_SEND,
     VAR_DEFAULT_PASSWORD,
+    VAR_TIME_RECEPTION_OF_APPS,
 )
