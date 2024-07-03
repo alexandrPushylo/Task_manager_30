624d014828e05d9aa7297757f7ce9ef07b370962
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-07-03 22:44:37 +0300

    Update application_material.py
    ref

--- dashboard/services/application_material.py
+++ dashboard/services/application_material.py
@@ -1,4 +1,5 @@
 from dashboard.models import Technic, User, ApplicationToday, WorkDaySheet, ApplicationMaterial
+from django.db.models import QuerySet
 import dashboard.assets as ASSETS
 
 from logger import getLogger
@@ -6,25 +7,32 @@ from logger import getLogger
 log = getLogger(__name__)
 
 
-def get_app_material(**kwargs) -> ApplicationMaterial | None:
+def get_app_material(**kwargs) -> ApplicationMaterial:
     try:
         application_material = ApplicationMaterial.objects.get(**kwargs)
         return application_material
     except ApplicationMaterial.DoesNotExist:
         log.error("get_app_material(): ApplicationMaterial.DoesNotExist")
-        return None
+
+
+def create_app_material(**kwargs) -> ApplicationMaterial:
+    try:
+        application_material = ApplicationMaterial.objects.create(**kwargs)
+        return application_material
+    except ValueError:
+        log.error("create_app_material(): ValueError")
 
 
 def get_apps_material_queryset(select_related: tuple = (),
                                order_by: tuple = (),
                                exclude: tuple = (),
-                               **kwargs) -> ApplicationMaterial.objects:
+                               **kwargs) -> QuerySet[ApplicationMaterial]:
     """
     :param exclude:
     :param order_by:
     :param select_related:
     :param kwargs: ApplicationMaterial.objects.filter(**kwargs)
-    :return: ApplicationMaterial.objects.filter
+    :return: QuerySet[ApplicationMaterial]
     """
 
     apps_material = ApplicationMaterial.objects.filter(**kwargs)
