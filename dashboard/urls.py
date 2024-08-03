f3ced5fb4c2f795fe2ad831e924d9fcd0a1826b4
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-08-03 19:11:37 +0300

    ref urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,4 +10,23 @@ urlpatterns = [
     path('work_days/', V.workday_sheet_view, name='work_days'),
     path('driver_sheet/', V.driver_sheet_view, name='driver_sheet'),
     path('technic_sheet/', V.technic_sheet_view, name='technic_sheet'),
+
+    path('conflicts_list/', V.conflicts_list_view, name='conflicts_list'),
+    path('conflict_resolution/', V.conflict_resolution_view, name='conflict_resolution'),
+
+    path('technic_application_list/', V.show_technic_application, name='technic_application_list'),
+    path('material_application_list/', V.show_material_application, name='material_application_list'),
+    path('material_application_supply/', V.material_application_supply_view, name='material_application_supply'),
+
+    path('users/', V.users_view, name='users'),
+    path('edit_user/', V.edit_user_view, name='edit_user'),
+    path('delete_user/', V.delete_user_view, name='delete_user'),
+    path('profile/', V.profile_view, name='profile'),
+
+    path('technics/', V.technic_view, name='technics'),
+    path('edit_technic/', V.edit_technic_view, name='edit_technic'),
+    path('delete_technic/', V.delete_technic_view, name='delete_technic'),
+
+    path('construction_site/', V.construction_site_view, name='construction_site'),
+    path('edit_construction_sites/', V.edit_construction_sites, name='edit_construction_sites'),
 ]
