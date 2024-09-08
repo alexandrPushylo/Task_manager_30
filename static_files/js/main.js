e047dbb52ddf276aa9d170816b4573e699f5dab1
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-09-08 12:16:35 +0300

    fix: btn_edit_technics_and_materials

--- static_files/js/main.js
+++ static_files/js/main.js
@@ -132,9 +132,7 @@ function onInput_tech_description(e) {
     const atID = e.id.replace('app_tech_description_', '');
     $('#div_btn_edit_control_' + atID).show();
     $('#btn_options_' + atID).show();
-    // $('#apply_' + atID).show();
-    // $('#reload_' + atID).show();
-    // $('#reject_' + atID).hide();
+    $('#btn_edit_technics_and_materials').hide();
 }
 
 $('.app_tech_description').each(function () {
@@ -240,6 +238,7 @@ function cancelAddedTechnic(e){
 
     $('#div_btn_edit_control_' + tech_sheet_id).hide();
     $('#btn_options_' + tech_sheet_id).show();
+    $('#btn_edit_technics_and_materials').show();
 }
 
 function reloadPage(e) {
