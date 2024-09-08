fe0db8dae404e675fedbf6ded0787dfd36e4b41e
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-09-08 12:58:32 +0300

    Update main.js

--- static_files/js/main.js
+++ static_files/js/main.js
@@ -134,7 +134,6 @@ function onInput_tech_description(e) {
     $('#btn_options_' + atID).show();
     $('#btn_edit_technics_and_materials').hide();
     $('#main_footer').hide();
-    e.scrollIntoView();
 }
 
 $('.app_tech_description').each(function () {
