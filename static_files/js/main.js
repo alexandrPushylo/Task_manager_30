da8b924411ce5c42520b7e8c38f7e14fa7c85f1a
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-09-17 15:28:56 +0300

    ref

--- static_files/js/main.js
+++ static_files/js/main.js
@@ -863,13 +863,6 @@ function changeReadOnlyMode(readOnly) {
     })
 }
 
-function checkChangesForEditApplication() {
-    const btn_cancel_for_edit_app = $('#btn_cancel_for_edit_app');
-    const btn_apply_for_edit_app = $('#btn_apply_for_edit_app');
-    btn_cancel_for_edit_app.hide();
-    btn_apply_for_edit_app.show();
-}
-
 function prepareModalDeleteApplication(url) {
     $('#btn_delete_app').attr('href', url);
 }
