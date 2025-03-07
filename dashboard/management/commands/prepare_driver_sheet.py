3473e320be76e1ac40110af940baf8c3d407f87d
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-03-07 20:34:12 +0300

    add: logger for com. manager

--- dashboard/management/commands/prepare_driver_sheet.py
+++ dashboard/management/commands/prepare_driver_sheet.py
@@ -6,9 +6,13 @@ class Command(BaseCommand):
     help = 'The Zen of Python'
 
     def handle(self, *args, **options):
+        from logger import getLogger
+        log = getLogger(__name__)
+
         from dashboard.services.work_day_sheet import get_workday
         from dashboard.services.driver_sheet import prepare_driver_sheet
         today_ = date.today()
         workday = get_workday(today_)
         if workday and workday.status:
             prepare_driver_sheet(workday)
+            log.info('CRON: prepare_driver_sheet()')
--- dashboard/management/commands/prepare_technic_sheet.py
+++ dashboard/management/commands/prepare_technic_sheet.py
@@ -6,9 +6,14 @@ class Command(BaseCommand):
     help = 'The Zen of Python'
 
     def handle(self, *args, **options):
+        from logger import getLogger
+
+        log = getLogger(__name__)
         from dashboard.services.work_day_sheet import get_workday
         from dashboard.services.technic_sheet import prepare_technic_sheets
         today_ = date.today()
         workday = get_workday(today_)
         if workday and workday.status:
             prepare_technic_sheets(workday)
+            log.info('CRON: prepare_technic_sheets()')
+
