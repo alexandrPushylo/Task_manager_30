17fa62e067385e49a7877058f9a35643d7f5ae2b
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-10-14 22:39:04 +0300

    add: logging about user is login

--- dashboard/views.py
+++ dashboard/views.py
@@ -549,6 +549,7 @@ def login_view(request):
                 user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            log.info(f"Пользователь {user.username} зашел в систему")
             return HttpResponseRedirect(ENDPOINTS.ROUTING_DASHBOARD)
         else:
             log.warning('Username or Password is incorrect')
