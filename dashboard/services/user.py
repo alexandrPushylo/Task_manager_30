a91c94840d23c87079ee75a7986a5cb16074b0ea
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-12-28 14:56:54 +0300

    Update user.py
    OOP

--- dashboard/services/user.py
+++ dashboard/services/user.py
@@ -1,10 +1,9 @@
 import enum
 
 from django.core.cache import cache
+from django.db import models
 from django.db.utils import IntegrityError
 from django.db.models import Q
-from django.contrib.auth.models import AbstractUser, AnonymousUser
-from pydantic import ValidationError
 
 from config.settings import USE_CACHE
 from dashboard.models import User
@@ -14,26 +13,27 @@ import dashboard.utilities as U
 from django.db.models import QuerySet  # type: ignore
 
 from dashboard.schemas.user_schema import UserSchema, EditUserSchema
+from dashboard.services.base import BaseService
 from logger import getLogger
 
 log = getLogger(__name__)
 
 
-class UserService:
+class UserService(BaseService):
     model = User
     schema = UserSchema
     CACHE_TTL = 10
-    USE_CACHE = USE_CACHE
 
     class CacheKeys(enum.Enum):
         DRIVER_LIST_FOR_SUPPLY = "driver_list_for_supply"
         CURRENT_USER = "current_user"
+        ALL_USER_LIST = "all_user_list"
 
     @classmethod
-    def get_user(cls, *args, **kwargs) -> User | None:
+    def get_object(cls, *args, **kwargs) -> User | None:
         try:
-            technic = cls.model.objects.get(*args, **kwargs)
-            return technic
+            obj = cls.model.objects.get(*args, **kwargs)
+            return obj
         except cls.model.DoesNotExist:
             log.warning(f"get_user({kwargs}): User.DoesNotExist ")
             return None
@@ -42,10 +42,10 @@ class UserService:
             return None
 
     @classmethod
-    def get_users_queryset(cls, *args, **kwargs) -> QuerySet[User]:
+    def get_queryset(cls, *args, **kwargs) -> QuerySet[User]:
         try:
-            users = cls.model.objects.filter(*args, **kwargs)
-            return users
+            queryset = cls.model.objects.filter(*args, **kwargs)
+            return queryset
         except ValueError:
             log.warning(f"get_users_queryset({kwargs}): ValueError")
             return cls.model.objects.none()
@@ -53,22 +53,26 @@ class UserService:
     @classmethod
     def get_current_user(cls, user_id) -> UserSchema | None:
         cache_key = f"{cls.CacheKeys.CURRENT_USER.value}:{user_id}"
+        cache_ttl = 60 * 60
+
         current_user_from_cache: UserSchema | None = cache.get(cache_key)
+        cache.touch(cache_key, cache_ttl)
         if current_user_from_cache is None:
-            current_user = cls.get_user(id=user_id)
+            current_user = cls.get_object(id=user_id)
             current_user_data = UserSchema(**current_user.to_dict())
             if cls.USE_CACHE:
-                cache.set(cache_key, current_user_data, cls.CACHE_TTL)
+                cache.set(cache_key, current_user_data, cache_ttl)
             return current_user_data
         return current_user_from_cache
 
     @classmethod
-    def add_new_user(cls, user_data: EditUserSchema) -> tuple[User | None, A.UserEditResult]:
+    def create(cls, user_data: EditUserSchema) -> tuple[User | None, A.UserEditResult]:
         validated_user_data = cls._prepare_user_data(user_data)
         if validated_user_data is None:
             return None, A.UserEditResult.ERROR
         try:
             new_user = cls.model.objects.create(**validated_user_data.model_dump())
+            cache.delete(cls.CacheKeys.ALL_USER_LIST.value)
             log.info(f"User {validated_user_data.last_name} has been added")
             return new_user, A.UserEditResult.OK
         except IntegrityError:
@@ -79,11 +83,11 @@ class UserService:
             return None, A.UserEditResult.ERROR
 
     @classmethod
-    def edit_user(cls, user_id: int, user_data: EditUserSchema) -> tuple[User | None, A.UserEditResult]:
+    def edit(cls, user_id: int, user_data: EditUserSchema) -> tuple[User | None, A.UserEditResult]:
         validate_data = cls._prepare_user_data(user_data)
         if validate_data is None:
             return None, A.UserEditResult.ERROR
-        user = cls.get_user(pk=user_id)
+        user = cls.get_object(pk=user_id)
         if user:
             user.username = validate_data.username
             user.first_name = validate_data.first_name
@@ -94,16 +98,18 @@ class UserService:
             user.post = validate_data.post
             user.supervisor_user_id = validate_data.supervisor_user_id
             user.save()
-            log.info(f"User {validate_data.title} [{validate_data.id_information}] has been edit")
+            cache.delete(cls.CacheKeys.ALL_USER_LIST.value)
+            log.info(f"User {user.last_name} {user.first_name} has been edit")
             return user, A.UserEditResult.OK
         return None, A.UserEditResult.ERROR
 
     @classmethod
-    def delete_user(cls, *args, **kwargs) -> bool:
-        user = cls.get_user(*args, **kwargs)
+    def delete(cls, *args, **kwargs) -> bool:
+        user = cls.get_object(*args, **kwargs)
         if user:
             user.isArchive = True
             user.save(update_fields=["isArchive"])
+            cache.delete(cls.CacheKeys.ALL_USER_LIST.value)
             log.info(f'User: ({user.last_name} {user.first_name}) has been archived')
             return True
         return False
@@ -115,9 +121,9 @@ class UserService:
             :param telephone:
             :return:
             """
-        validate_telephone = U.validate_telephone(telephone, length=7, use_pref=False)
+        validate_telephone = cls.validate_telephone(telephone, length=7, use_pref=False)
         if validate_telephone:
-            user = cls.get_users_queryset(
+            user = cls.get_queryset(
                 isArchive=False, telephone__contains=validate_telephone
             )
         else:
@@ -138,7 +144,7 @@ class UserService:
         post = user_data.post if user_data.post is not None else A.UserPosts.EMPLOYEE.title
         supervisor_user_id = int(user_data.supervisor_user_id) if user_data.supervisor_user_id is not None else None
 
-        telephone = U.validate_telephone(raw_telephone)
+        telephone = cls.validate_telephone(raw_telephone)
 
         if all((username, first_name, last_name, password)):
             log.info("Data: (username, first_name, last_name, password) is Ok")
@@ -156,15 +162,68 @@ class UserService:
             return None
 
     @classmethod
-    def is_user_exists(cls, user_data: EditUserSchema) -> bool:
-        fnd_user = cls.model.objects.filter(
+    def is_exists(cls, user_data: EditUserSchema) -> bool:
+        user_is_exists = cls.get_queryset(
             Q(username=user_data.username)
             | Q(telephone__isnull=False, telephone=user_data.telephone)
             | Q(first_name=user_data.first_name, last_name=user_data.last_name)
-        )
-        return fnd_user.exists()
+        ).exists()
+        return user_is_exists
 
+    @classmethod
+    def validate_telephone(cls, telephone: str, length=9, use_pref=True) -> str | None:
+        """
+        Валидация номера телефона
+        :param use_pref:
+        :param length:
+        :param telephone:
+        :return:
+        """
+        if telephone:
+            pref = "+375"
+            out = [sym for sym in telephone if sym in "0123456789"]
+            out = "".join(out)
 
+            if len(out) >= length:
+                if use_pref:
+                    return pref + out[-length:]
+                else:
+                    return out[-length:]
+            else:
+                return None
+        return None
+
+    @classmethod
+    def get_driver_list(cls) -> list[UserSchema | None]:
+        all_users_list = cls.get_all_users_list()
+        if all_users_list:
+            driver_list_data = [user for user in all_users_list if user.post == A.UserPosts.DRIVER.title]
+            return driver_list_data
+        return []
+
+    @classmethod
+    def get_foreman_list(cls) -> list[UserSchema | None]:
+        all_users_list = cls.get_all_users_list()
+        if all_users_list:
+            foreman_list_data = [user for user in all_users_list if user.post == A.UserPosts.FOREMAN.title]
+            return foreman_list_data
+        return []
+
+    @classmethod
+    def get_all_users_list(cls) -> list[UserSchema | None]:
+        cache_key = f"{cls.CacheKeys.ALL_USER_LIST.value}"
+        cache_ttl = 60 * 60
+
+        all_users_list_from_cache = cache.get(cache_key)
+        if all_users_list_from_cache is None:
+            all_users_list = cls.get_queryset(isArchive=False)
+            all_users_list_data = [UserSchema(**user.to_dict()) for user in all_users_list]
+            if cls.USE_CACHE:
+                cache.set(cache_key, all_users_list_data, cache_ttl)
+            return all_users_list_data
+        else:
+            cache.touch(cache_key, cache_ttl)
+            return all_users_list_from_cache
 
 
 def is_administrator(user: User) -> bool:
@@ -195,51 +254,51 @@ def is_employee(user: User) -> bool:
     return True if user.post == A.UserPosts.EMPLOYEE.title else False
 
 
-def get_foreman(user: User) -> User | None:
-    """
-    Получить объект типа: foreman от user или None
-    :param user: foreman or master
-    :return: foreman: User | None
-    """
-    if is_foreman(user):
-        return user
-    elif is_master(user):
-        foreman = get_user(pk=user.supervisor_user_id)
-        return foreman
-    else:
-        return None
+# def get_foreman(user: User) -> User | None:
+#     """
+#     Получить объект типа: foreman от user или None
+#     :param user: foreman or master
+#     :return: foreman: User | None
+#     """
+#     if is_foreman(user):
+#         return user
+#     elif is_master(user):
+#         foreman = get_user(pk=user.supervisor_user_id)
+#         return foreman
+#     else:
+#         return None
 
 
-def get_user(*args, **kwargs) -> UserSchema | None:#####
-    cache_key = U.validate_cache_name(f"get_user:{args},{kwargs}")
-    cache_timeout = 10
-    try:
-        cache_user: UserSchema | None = cache.get(cache_key)
-        if cache_user is None:
-            user = User.objects.get(*args, **kwargs)
-            user_scheme = UserSchema(**user.to_dict())
-            cache.set(cache_key, user_scheme, cache_timeout) if USE_CACHE else None
-            return user_scheme
-        return cache_user
-    except User.DoesNotExist:
-        log.warning(f"get_user({kwargs}): User.DoesNotExist ")
-        return None
-    except ValueError:
-        log.error(f"get_user({kwargs}): ValueError")
-        return None
+# def get_user(*args, **kwargs) -> UserSchema | None:#####
+#     cache_key = U.validate_cache_name(f"get_user:{args},{kwargs}")
+#     cache_timeout = 10
+#     try:
+#         cache_user: UserSchema | None = cache.get(cache_key)
+#         if cache_user is None:
+#             user = User.objects.get(*args, **kwargs)
+#             user_scheme = UserSchema(**user.to_dict())
+#             cache.set(cache_key, user_scheme, cache_timeout) if USE_CACHE else None
+#             return user_scheme
+#         return cache_user
+#     except User.DoesNotExist:
+#         log.warning(f"get_user({kwargs}): User.DoesNotExist ")
+#         return None
+#     except ValueError:
+#         log.error(f"get_user({kwargs}): ValueError")
+#         return None
 
 
-def get_user_queryset(*args, **kwargs) -> list[UserSchema | None]:######
-    cache_key = U.validate_cache_name(f"get_user_queryset:{args},{kwargs}")
-    cache_timeout = 10
-
-    cache_user: list[UserSchema | None] = cache.get(cache_key)
-    if cache_user is None:
-        user_queryset = User.objects.filter(*args, **kwargs)
-        user_queryset_schema = [UserSchema(**user.to_dict()) for user in user_queryset]
-        cache.set(cache_key, user_queryset_schema, cache_timeout) if USE_CACHE else None
-        return user_queryset_schema
-    return cache_user
+# def get_user_queryset(*args, **kwargs) -> list[UserSchema | None]:######
+#     cache_key = U.validate_cache_name(f"get_user_queryset:{args},{kwargs}")
+#     cache_timeout = 10
+#
+#     cache_user: list[UserSchema | None] = cache.get(cache_key)
+#     if cache_user is None:
+#         user_queryset = User.objects.filter(*args, **kwargs)
+#         user_queryset_schema = [UserSchema(**user.to_dict()) for user in user_queryset]
+#         cache.set(cache_key, user_queryset_schema, cache_timeout) if USE_CACHE else None
+#         return user_queryset_schema
+#     return cache_user
 
 
 # def edit_user(user_id, data: EditUserSchema) -> tuple[User | None, A.UserEditResult]:
