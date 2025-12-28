466711409eebedb18c8540390f487e886bb441f0
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-12-28 14:51:07 +0300

    Update construction_site_schema.py

--- dashboard/schemas/construction_site_schema.py
+++ dashboard/schemas/construction_site_schema.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 
 from pydantic import BaseModel, Field
 
@@ -7,7 +7,7 @@ class ConstructionSiteSchema(BaseModel):
     id: int
     address: str = Field(max_length=512, description="Адрес")
     foreman: int | None = Field(description="Прораб")
-    deleted_date: datetime | None = Field(description="Дата удаления")
+    deleted_date: date | None = Field(description="Дата удаления")
     status: bool = Field(description="Статус объекта")
     isArchive: bool = Field(description="Архивирован?")
 
